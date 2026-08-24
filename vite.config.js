d11344b9cbe38de60e78bb3724cc4b0a6947ff1b
eslam985 <ee17172@gmail.com>
2026-08-25 00:54:43 +0300

    fix config

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,29 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  base: '/egyPyramid-dashboard-in_githubPage/',
+  base: "/egyPyramid-dashboard-in_githubPage/",
   plugins: [vue(), tailwindcss()],
   build: {
-    outDir: 'dist', 
+    outDir: "dist",
     emptyOutDir: true,
-    minify: 'esbuild', 
+    minify: "esbuild",
     sourcemap: false,
-    target: 'esnext'
+    target: "esnext",
+    // 🌟 أضف هذا الجزء لتغيير نمط أسماء الملفات وحذف أي شرطة سفلية تسبب 404
+    rollupOptions: {
+      output: {
+        sanitizeFileName(name) {
+          return name.replace(/[^a-zA-Z0-9-.]/g, ""); // يحذف الحروف الغريبة والشرطات السفلية المتداخلة
+        },
+        chunkFileNames: "assets/chunk-[name]-[hash].js",
+        entryFileNames: "assets/entry-[name]-[hash].js",
+        assetFileNames: "assets/asset-[name]-[hash].[ext]",
+      },
+    },
   },
   esbuild: {
-    target: 'esnext'
-  }
-})
+    target: "esnext",
+  },
+});
